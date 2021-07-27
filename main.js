6304975e50b7e0a6322a089ff717c1cf80f2102f
danielo <danielo@buffiniandcompany.com>
2021-07-27 14:00:14 -0700

    added back handleSkillNavToggle

--- main.js
+++ main.js
@@ -41,7 +41,23 @@ function highlightNavOnScroll() {
   window.addEventListener('scroll', debounce(handleScroll, 20));
 }
 
+function handleSkillNavToggle() {
+  const skillLinks = document.querySelectorAll('[data-skill-group-target]');
+  const skillSections = document.querySelectorAll('[data-skill-group]');
+  const handleLinkClicks = (e) => {
+    e.preventDefault();
+    const target = e.target.dataset.skillGroupTarget;
+    skillLinks.forEach((link) => link.classList.remove('is-active'));
+    skillSections.forEach((section) => section.classList.remove('is-active'));
+    document.querySelector(`[data-skill-group-target="${target}"]`).classList.add('is-active');
+    document.querySelector(`[data-skill-group="${target}"]`).classList.add('is-active');
+  };
+
+  skillLinks.forEach((link) => link.addEventListener('click', handleLinkClicks));
+}
+
 (function () {
   useSmoothScrolling();
   highlightNavOnScroll();
+  handleSkillNavToggle();
 })();
